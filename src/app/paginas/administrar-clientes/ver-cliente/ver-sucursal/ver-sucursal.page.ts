5a0c0dbbcc5171f4a657d7cb0e3b044d1837d338
Santiago Acevedo <santiagoacevedo94@gmail.com>
2021-08-12 09:59:32 -0300

    ips

--- src/app/paginas/administrar-clientes/ver-cliente/ver-sucursal/ver-sucursal.page.ts
+++ src/app/paginas/administrar-clientes/ver-cliente/ver-sucursal/ver-sucursal.page.ts
@@ -494,7 +494,7 @@ imprimir(qrs){
 
 test($event) {
   console.warn($event)
-  // window.open('http://192.168.0.71:3000/getZip')
+  // window.open('http://157.230.90.222:3000/getZip')
 }
 
 descargar(){
@@ -504,7 +504,7 @@ descargarSucursal(){
   // console.warn(1)
   var link = document.createElement("a");
   link.download = this.sucursal.sucursal[0].id_sucursal + ".png";
-  link.href = "http://192.168.0.71:3000/getfile/sucursales/"+this.sucursal.sucursal[0].id_sucursal ;
+  link.href = "http://157.230.90.222:3000/getfile/sucursales/"+this.sucursal.sucursal[0].id_sucursal ;
   link.click();
 }
 
@@ -513,7 +513,7 @@ descargarQrEquipo(equipo){
   // console.warn(equipo.id_equipo)
   var link = document.createElement("a");
   link.download = equipo.id_equipo + ".png";
-  link.href = "http://192.168.0.71:3000/getfile/equipos/"+equipo.id_equipo;
+  link.href = "http://157.230.90.222:3000/getfile/equipos/"+equipo.id_equipo;
   link.click();
 }
 
@@ -530,7 +530,7 @@ descargarQrThisWorkstation(grupoEquipo){
   }
   var link = document.createElement("a");
   link.download = "qrs.png";
-  link.href = "http://192.168.0.71:3000/getZip?type=equipos&name="+grupoEquipo.nombre_equipo_grupo+"&data="+ids;
+  link.href = "http://157.230.90.222:3000/getZip?type=equipos&name="+grupoEquipo.nombre_equipo_grupo+"&data="+ids;
   link.click();
 }
 
@@ -553,7 +553,7 @@ descargarAllQrSucursal(){
   }
   var link = document.createElement("a");
   link.download = "qrs.png";
-  link.href = "http://192.168.0.71:3000/getZip?type=all&name="+this.sucursal.sucursal[0].razon_social_sucursal+"&data="+ids;
+  link.href = "http://157.230.90.222:3000/getZip?type=all&name="+this.sucursal.sucursal[0].razon_social_sucursal+"&data="+ids;
   link.click();
 
 }
@@ -576,7 +576,7 @@ descargarQrAllWorkstations(){
   }
   var link = document.createElement("a");
   link.download = "qrs.png";
-  link.href = "http://192.168.0.71:3000/getZip?type=equipos&name=equipos_"+this.sucursal.sucursal[0].razon_social_sucursal+"&data="+ids;
+  link.href = "http://157.230.90.222:3000/getZip?type=equipos&name=equipos_"+this.sucursal.sucursal[0].razon_social_sucursal+"&data="+ids;
   link.click();
 }
 
