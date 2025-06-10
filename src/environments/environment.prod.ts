95fcaa4addbdd434ad9981ed68f4c7027641bc97
niic0o <nicosena10@gmail.com>
2025-06-09 21:34:31 -0300

    fix: URL backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   api:{
-    base_url:"http://45.227.161.79:3000/"
+    //base_url:"http://45.227.161.79:3000/"
+    base_url:"https://backend.desinfeccionescpa.com.ar/"
   }
 };
